d328a1a16390dcfbe8070b7de62f934b9ba3b07f
gangintheremark <nkh9587@naver.com>
2024-01-06 22:16:15 +0900

    setting

---
+++ src/main/java/com/algorithm/Algonote/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.algorithm.Algonote.controller;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+
+    @RequestMapping("/main")
+    public String goMain() {
+        return "main";
+    }
+}
