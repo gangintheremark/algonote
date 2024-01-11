75a4db5833f7a1ddce4917ac1d32cd7034326be2
onegqueen <rkdwldnjs30@naver.com>
2024-01-11 23:04:46 +0900

    #2 feature : member entity 및 repository 추가

---
+++ src/main/java/com/algorithm/Algonote/dto/JwtToken.java
@@ -0,0 +1,12 @@
+package com.algorithm.Algonote.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class JwtToken {
+
+}
--- src/main/java/com/algorithm/Algonote/model/MemberEntity.java
+++ src/main/java/com/algorithm/Algonote/model/MemberEntity.java
@@ -1,5 +1,33 @@
 package com.algorithm.Algonote.model;
 
-public class MemberEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="problem")
+@Entity
+public class MemberEntity {
+    @Id
+    @Column(name = "member_id")
+    private String id;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name="nickname")
+    private String nickname;
 }
