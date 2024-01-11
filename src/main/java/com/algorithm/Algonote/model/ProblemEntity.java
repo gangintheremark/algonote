3ce37bca54bd1af2c8d665617a3e1bac7e098007
gyeong_min <81904943+gangintheremark@users.noreply.github.com>
2024-01-11 15:05:25 +0900

    #4 ADD : problem 테이블 컬럼 매핑

--- src/main/java/com/algorithm/Algonote/model/ProblemEntity.java
+++ src/main/java/com/algorithm/Algonote/model/ProblemEntity.java
@@ -13,15 +13,40 @@ import org.springframework.stereotype.Service;
 @Table(name="problem")
 @Entity
 public class ProblemEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long problemNum;
-    private String userid;
-    private String title;
-    private String text;
-    private String code;
-    private String solved;
-    private String language;
-    private String problemSite;
-    private String difficulty;
-    private String category;
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "problemNum")
+	    private int problemNum;
+
+	    @Column(name = "userid", length = 20)
+	    private String userid;
+
+	    @Column(name = "title", length = 50, nullable = false)
+	    private String title;
+
+	    @Column(name = "text", length = 4000)
+	    private String text;
+
+	    @Column(name = "code", length = 30000)
+	    private String code;
+
+	    @Column(name = "solved", length = 20, nullable = false)
+	    private String solved;
+
+	    @Column(name = "language", length = 10, nullable = false)
+	    private String language;
+
+	    @Column(name = "problemSite", length = 20, nullable = false)
+	    private String problemSite;
+
+	    @Column(name = "difficulty", length = 20, nullable = false)
+	    private String difficulty;
+
+	    @Column(name = "category", length = 30)
+	    private String category;
+
+//	    @ManyToOne
+//	    @JoinColumn(name = "userid", referencedColumnName = "userid", insertable = false, updatable = false)
+//	    private Member member;
+
 }
