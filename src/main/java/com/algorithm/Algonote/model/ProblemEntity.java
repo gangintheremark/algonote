db7d2f411b51be358bf0e4258da0fc797cdf4a4c
gangintheremark <nkh9587@naver.com>
2024-01-14 15:53:49 +0900

    :bug: #13 fix : Data too long for column 해결 ( 데이터타입을 LONGTEXT로 변경 )

--- src/main/java/com/algorithm/Algonote/model/ProblemEntity.java
+++ src/main/java/com/algorithm/Algonote/model/ProblemEntity.java
@@ -4,46 +4,50 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Builder
 @Setter
 @Getter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="problem")
+@Table(name = "problem")
 @Entity
 public class ProblemEntity {
-	 @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    @Column(name = "problemNum")
-	    private int problemNum;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "problemNum")
+    private int problemNum;
 
-	    @Column(name = "userid", length = 20)
-	    private String userid;
+    @Column(name = "userid", length = 20)
+    private String userid;
 
-	    @Column(name = "title", length = 50, nullable = false)
-	    private String title;
+    @Column(name = "title", length = 50, nullable = false)
+    private String title;
 
-	    @Column(name = "text", length = 4000)
-	    private String text;
+    @Column(name = "text", columnDefinition = "LONGTEXT")
+    private String text;
 
-	    @Column(name = "code", length = 30000)
-	    private String code;
+    @Column(name = "code", columnDefinition = "LONGTEXT")
+    private String code;
 
-	    @Column(name = "solved", length = 20, nullable = false)
-	    private String solved;
+    @Column(name = "solved", length = 20, nullable = false)
+    private String solved;
 
-	    @Column(name = "language", length = 10, nullable = false)
-	    private String language;
+    @Column(name = "language", length = 10, nullable = false)
+    private String language;
 
-	    @Column(name = "problemSite", length = 20, nullable = false)
-	    private String problemSite;
+    @Column(name = "problemSite", length = 20, nullable = false)
+    private String problemSite;
 
-	    @Column(name = "difficulty", length = 20, nullable = false)
-	    private String difficulty;
+    @Column(name = "difficulty", length = 20, nullable = false)
+    private String difficulty;
 
-	    @Column(name = "category", length = 30)
-	    private String category;
+    @ElementCollection
+    @CollectionTable(name = "problem_categories")
+    @Column(name = "category")
+    private List<String> category;
 
 //	    @ManyToOne
 //	    @JoinColumn(name = "userid", referencedColumnName = "userid", insertable = false, updatable = false)
