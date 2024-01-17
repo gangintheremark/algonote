d7c960f8beb149371e87e49804538a888af513c5
gangintheremark <nkh9587@naver.com>
2024-01-17 14:18:23 +0900

    🧹#37 : chore : memberId 가져오기

--- src/main/java/com/algorithm/Algonote/controller/RegistController.java
+++ src/main/java/com/algorithm/Algonote/controller/RegistController.java
@@ -5,39 +5,53 @@ import com.algorithm.Algonote.service.ProblemService;
 import jakarta.annotation.PostConstruct;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.beans.factory.parsing.Problem;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class RegistController {
 
-    private final ProblemService problemService;
+	private final ProblemService problemService;
+	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-    @PostMapping("/problem")
-    public String registProblem(ProblemEntity request) {
-            // 로그인 체크 인터셉터 추가 필요
-            request.setUserid("gang"); // [임시]
-            problemService.addProblem(request);
-            return "problem/success";
-    }
-    
-	
+	@PostMapping("/problem")
+	public String registProblem(ProblemEntity request, Principal principal) {
+		String memberId = principal.getName();
+		request.setUserid(memberId);
+		problemService.addProblem(request);
+		return "problem/success";
+	}
+
 	@PutMapping("/modify")
 	@ResponseBody
-	public void modify(ProblemEntity request) {
-        // 로그인 체크 인터셉터 추가 필요
-        request.setUserid("gang"); // [임시]
-        System.out.println("요청: " + request);
-        problemService.modify(request);
+	public void modify(ProblemEntity request, Principal principal) {
+		String memberId = principal.getName();
+		request.setUserid(memberId);
+		problemService.modify(request);
+	}
+
+	@RequestMapping("/goModify")
+	public String goModify(@RequestParam("problemNum") int problemNum, Model m) {
+		ProblemEntity problem = problemService.findProblem(problemNum);
+		m.addAttribute("problem", problem);
+		return "problem/reviseForm";
 	}
 }
